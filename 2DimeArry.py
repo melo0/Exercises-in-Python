c16aa2e698bd21acaff5116cae3e1b3c80d6ccee
melo0 <melo0@op.pl>
2017-07-27 10:53:35 +0200

    Update 2DimeArry.py
    Shortened record

--- 2DimeArry.py
+++ 2DimeArry.py
@@ -1,7 +1,7 @@
-x = [z for z in raw_input().split(',')]
+x = [int(z) for z in raw_input().split(',')]
 result = []
-for i in range( 0, int(x[0])):
+for i in range( 0, x[0]):
 	result.append([])
-	for j in range(0, int(x[1])):
+	for j in range(0, x[1]):
 		result[i].append(i*j)
 print result
