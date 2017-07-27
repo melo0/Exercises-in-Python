f6eda95797d38e7a165d9496b3c30a50fc05df07
melo0 <melo0@op.pl>
2017-07-27 08:48:32 +0200

    Update sqrt.py

--- sqrt.py
+++ sqrt.py
@@ -1,6 +1,9 @@
 # sqrt of formula
 import math
+resault = []
 h = [x for x in raw_input().split(',')]
 c=50
 d=30
-math.sqrt((2*c*d)/h)
+for i in h:
+  resault.append = math.sqrt((2*c*d)/i)
+print(resault)
