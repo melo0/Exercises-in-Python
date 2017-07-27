a7dcd0b6eb411d18f9e2c6b1e70405478c7a973d
melo0 <melo0@op.pl>
2017-07-27 09:18:32 +0200

    Update sqrt.py
    working script

--- sqrt.py
+++ sqrt.py
@@ -5,5 +5,5 @@ h = [x for x in raw_input().split(',')]
 c=50
 d=30
 for i in h:
-  resault.append = math.sqrt((2*c*d)/i)
-print(resault)
+	resault.append(str(int(math.sqrt((2*c*(float(i)))/d))))
+print ','.join(resault)
